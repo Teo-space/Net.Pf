e67d2d2dfca3d7d53d861fb3b1a35cd34037a6f3
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-07 21:18:44 +0300

    bootstrap forms

---
+++ Net.Pf/Pages/Bootstrap/Forms3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Net.Pf.Pages.Bootstrap
+{
+    public class Forms3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
