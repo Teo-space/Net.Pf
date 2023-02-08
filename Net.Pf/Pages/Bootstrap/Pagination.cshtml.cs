6b1a06cfc0854b32620bc88b806b4410bc6579dd
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-08 07:24:35 +0300

    Bootstrap Pagination

---
+++ Net.Pf/Pages/Bootstrap/Pagination.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Net.Pf.Pages.Bootstrap
+{
+    public class PaginationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
