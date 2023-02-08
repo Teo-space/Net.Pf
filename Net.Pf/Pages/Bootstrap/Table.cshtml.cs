641ad3951a8af3ae7a5fa2eff15b313f87e23392
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-08 22:45:02 +0300

    layout up

---
+++ Net.Pf/Pages/Bootstrap/Table.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Net.Pf.Pages.Bootstrap
+{
+    public class TableModel : PageModel
+    {
+        public DataTable dataTable = new();
+        public void OnGet()
+        {
+            dataTable.Columns.Add("Name");
+            dataTable.Columns.Add("Value");
+
+
+            for(int i = 0; i < 30; i++)
+            {
+                var row = dataTable.NewRow();
+
+                foreach(DataColumn column in dataTable.Columns)
+                {
+                    row[column] = $"{i}";
+                }
+                dataTable.Rows.Add(row);
+            }
+
+        }
+    }
+}
