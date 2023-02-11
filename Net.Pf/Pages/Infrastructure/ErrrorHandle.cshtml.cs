6cddbcb67bce9a21738fc200e769af5a9013b809
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-11 21:20:04 +0300

    Pages

--- Net.Pf/Pages/Infrastructure/ErrrorHandle.cshtml.cs
+++ Net.Pf/Pages/Infrastructure/ErrrorHandle.cshtml.cs
@@ -12,6 +12,6 @@ public class ErrrorHandleModel : PageModel
     {
         await Task.Delay(1);
 
-        throw new Exception("ErrrorHandleModel Test");
+        throw new Exception("Testing Exception");
     }
 }
