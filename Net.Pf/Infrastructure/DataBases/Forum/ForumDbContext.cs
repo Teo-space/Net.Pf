5a10922533a3e51730520884748c0853702dd311
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-16 21:11:35 +0300

    temp

--- Net.Pf/Infrastructure/DataBases/Forum/ForumDbContext.cs
+++ Net.Pf/Infrastructure/DataBases/Forum/ForumDbContext.cs
@@ -1,8 +1,25 @@
-﻿namespace Net.Pf.Infrastructure.DataBases.Forum;
+﻿using Net.Pf.Identity;
+
+namespace Net.Pf.Infrastructure.DataBases.Forum;
 
 public class ForumDbContext : DbContext
 {
+    //public DbSet<AppIdentityUser> Users { get; set; }
+
+    public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
 
 
+    /*
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+    */
 
+
+    //ForumSections
 }
---
+++ Net.Pf/Infrastructure/DataBases/Forum/Models/ForumSections.cs
@@ -0,0 +1,38 @@
+﻿namespace Net.Pf.Infrastructure.DataBases.Forum.Models;
+
+
+public class ForumSections
+{
+    public Guid ForumSectionsId { get; set; }
+
+    public uint Order { get; set; }
+
+    public string Name { get; set; }
+
+
+
+
+}
+
+
+public static class ExtensionsEnum
+{
+    //public static readonly ForumSections[]
+}
+
+
+
+
+
+
+/*
+
+		public Guid Id { get; set; }
+
+
+		public Guid CreatorId { get; set; }
+        public string CreatorName { get; set; }
+        public string CreatorEmail { get; set; }
+        public DateTime Created { get; set; }
+
+*/
