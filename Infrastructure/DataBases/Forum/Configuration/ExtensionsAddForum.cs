c91098450db8d8435e7b4aef393822cd9ed721b4
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-25 00:19:30 +0300

    init

---
+++ Infrastructure/DataBases/Forum/Configuration/ExtensionsAddForum.cs
@@ -0,0 +1,33 @@
+﻿using Extensions.Configuration;
+using Infrastructure.DataBases.Forum.DbContexts;
+using Infrastructure.DataBases.Forum.Managers.ForkManager;
+using Infrastructure.DataBases.Forum.Managers.PostManager;
+using Infrastructure.DataBases.Forum.Managers.TopicManager;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DataBases.Forum.Configuration;
+
+public static class ExtensionsAddForum
+{
+    public static IServiceCollection AddForum(this IServiceCollection services)
+    {
+        //ForumDbContext
+        services.AddDbContext<ForumDbContext>(options =>
+        {
+            //options.UseSqlServer(connectionString)
+            //options.UseInMemoryDatabase("IdentityDb");
+            options.UseSqlite($"Data Source=Infrastructure/DataBases/Forum/Data/ForumDbContext.db");
+        });
+
+        services.AddUserAccessor();
+        services.AddScoped<IPostManager, PostManager>();
+        services.AddScoped<ITopicManager, TopicManager>();
+        services.AddScoped<IForkManager, ForkManager>();
+
+        return services;
+    }
+
+
+}
+
+
