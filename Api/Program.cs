ca8b1a660f00e904adf02c1ae6f58199950bfc29
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-24 09:34:53 +0300

    cleanup

--- Api/Program.cs
+++ Api/Program.cs
@@ -36,60 +36,7 @@ public static class WebApplicationConfiguration
         app.UseAuthorization();
         app.MapControllers();
 
-        //app.MapGet("/Hello", () => "Hello World!").RequireAuthorization();
-        //app.MapGet("/Hello", (string a) => $"Hello World {a}").RequireAuthorization();
-
-
-        //app.Route("Fluent").WithArgument("test").Build();
-
         return app;
     }
 }
 
-
-public static class ExtensionsEndpointConventionBuilder
-{
-    public static EndpointConventionBuilder Route
-        (this IEndpointRouteBuilder endpoints, string route) => new EndpointConventionBuilder(endpoints, route);
-
-
-}
-
-
-public record EndpointConventionBuilder(IEndpointRouteBuilder endpoints, string Route)
-{
-    public EndpointConventionBuilder Create(string route) => new(endpoints, route);
-
-    public EndpointConventionBuilderWithArgument<T> WithArgument<T>(T arg) 
-        => new EndpointConventionBuilderWithArgument<T>(endpoints, Route, arg);
-}
-
-
-
-
-public record EndpointConventionBuilderWithArgument<TArgument>
-    (IEndpointRouteBuilder endpoints, string Route, TArgument Argument) 
-    : EndpointConventionBuilder(endpoints, Route)
-{
-    public IEndpointConventionBuilder Build()
-    {
-        return endpoints.MapGet(Route, (TArgument Argument) => $"Res: {Argument}");
-    }
-}
-
-
-
-public record EndpointConventionBuilderResult<TArgument>
-
-    (IEndpointRouteBuilder endpoints, string Route, TArgument Argument)
-
-    : EndpointConventionBuilderWithArgument<TArgument>(endpoints, Route, Argument)
-
-{
-    public IEndpointConventionBuilder Build<TBuilder>() where TBuilder : IEndpointConventionBuilder
-    {
-        return endpoints.MapGet(Route, (TArgument Argument) => $"Res: {Argument}");
-    }
-
-
-}
