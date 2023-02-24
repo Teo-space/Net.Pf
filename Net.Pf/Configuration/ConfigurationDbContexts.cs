0636a45f4889c3d1db01c9a000fce2b120c711e4
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-25 00:19:49 +0300

    cleanup

--- Net.Pf/Configuration/ConfigurationDbContexts.cs
+++ Net.Pf/Configuration/ConfigurationDbContexts.cs
@@ -1,19 +1,11 @@
-﻿using Net.Pf.DataBases.Forum;
-using Net.Pf.Identity;
-
-namespace Net.Pf.Configuration;
+﻿namespace Net.Pf.Configuration;
 
 
 public static class ConfigurationDbContexts
 {
     public static void AddDbContexts(this IServiceCollection services)
     {
-        services.AddDbContext<ForumDbContext>(options =>
-        {
-            //options.UseSqlServer(connectionString)
-            //options.UseInMemoryDatabase("IdentityDb");
-            options.UseSqlite($"Data Source=Infrastructure/DataBases/Forum/ForumDbContext.db");
-        });
+
 
 
 
