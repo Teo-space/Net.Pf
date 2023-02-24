47357bbcc66611384373065a98b0d12a09c97b0f
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-25 01:55:29 +0300

    add method

--- Extensions/ExtensionsClaims.cs
+++ Extensions/ExtensionsClaims.cs
@@ -1,16 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 
 namespace Extensions;
 
 
 public static class ExtensionsClaims
 {
-    public static string Claim(this ClaimsPrincipal principal, string claimType)
-    {
-        return principal.FindFirstValue(claimType);
-    }
-
+    public static string Claim(this ClaimsPrincipal principal, string claimType) => principal.FindFirstValue(claimType);
 
     public static string ClaimName(this ClaimsPrincipal principal) 
         => principal.Claim(ClaimTypes.Name);
