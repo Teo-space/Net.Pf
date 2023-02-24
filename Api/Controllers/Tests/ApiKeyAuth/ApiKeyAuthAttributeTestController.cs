cdb321c39815b2b332880d37116dab1877494fee
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-24 09:33:14 +0300

    cleanup

---
+++ Api/Controllers/Tests/ApiKeyAuth/ApiKeyAuthAttributeTestController.cs
@@ -0,0 +1,26 @@
+﻿using Api.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.Tests.ApiKeyAuth
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiKeyAuthAttributeTestController : ControllerBase
+    {
+        private readonly ILogger<ApiKeyAuthAttributeTestController> _logger;
+
+        public ApiKeyAuthAttributeTestController(ILogger<ApiKeyAuthAttributeTestController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [ApiKeyAuth]
+        [HttpGet]
+        public string Get() => "Result";
+
+
+    }
+}
+
+
