f667e3021205cefb828ca5c67a3d8f1ba8639d95
iTeo <barmin.andrey.nnov@gmail.com>
2023-02-25 12:18:07 +0300

    init

---
+++ Net.Pf/Pages/Bootstrap/Text/LayoutTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Net.Pf.Pages.Bootstrap.Text
+{
+    public class LayoutTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
